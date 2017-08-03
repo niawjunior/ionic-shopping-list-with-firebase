9d23bb84db385946f1c5612aee68d27cadf5e61f
niawkung <niawkung2@gmail.com>
2017-08-03 12:42:04 +0700

    Add input text

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ShoppingListPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
+import { AddShoppingPage } from '../add-shopping/add-shopping';
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
@@ -17,8 +10,7 @@ export class ShoppingListPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
+  navigateToAddShoppingPage() {
+    this.navCtrl.push(AddShoppingPage);
   }
-
 }
