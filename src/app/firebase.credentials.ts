303a8cb5aa0ee8cb58470eb528adfbb0a986bedf
niawkung <niawkung2@gmail.com>
2017-08-03 11:49:45 +0700

    generate page ShoppingList

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyD_LwW0Bs2Q-QNKahvFnZ7Bc-sSDAxhozQ",
+    authDomain: "fir-crud-96a90.firebaseapp.com",
+    databaseURL: "https://fir-crud-96a90.firebaseio.com",
+    projectId: "fir-crud-96a90",
+    storageBucket: "fir-crud-96a90.appspot.com",
+    messagingSenderId: "72763769660"
+  };
