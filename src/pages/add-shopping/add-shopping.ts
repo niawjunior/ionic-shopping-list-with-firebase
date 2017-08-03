0677d1c4163e48be75be8adf514f22c7d0f45ca7
niawkung <niawkung2@gmail.com>
2017-08-03 12:59:25 +0700

    fix

--- src/pages/add-shopping/add-shopping.ts
+++ src/pages/add-shopping/add-shopping.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
 @Component({
   selector: 'page-add-shopping',
   templateUrl: 'add-shopping.html',
 })
 export class AddShoppingPage {
-
+  shoppingItem = {} as ShoppingItem
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddShoppingPage');
+  addShoppingItem(shoppingItem: ShoppingItem) {
+    console.log(shoppingItem);
   }
-
 }
